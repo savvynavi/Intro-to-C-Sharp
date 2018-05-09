c76841a5c2867a311d3f4d94aa0036ad4f8abf09
Georgia Shepherd <fruitbat_08@hotmail.com>
2018-05-09 12:39:08 +0930

    everything but deleting tiles/saving/loading done

--- RoomEditor/RoomEditor/RoomForm.cs
+++ RoomEditor/RoomEditor/RoomForm.cs
@@ -14,8 +14,19 @@ namespace RoomEditor {
 		Spritesheet spritesheet = null;
 		Bitmap drawArea = null;
 
+		public Point CurrentTile { get; private set; } = new Point();
+
+		struct Tiles {
+			public Point SourceCoord;
+			public Point DestinationCoord;
+		}
+
+		List<Tiles> drawSpriteAtPos;
+
 		public RoomForm() {
 			InitializeComponent();
+			drawSpriteAtPos = new List<Tiles>();
+
 			drawArea = new Bitmap(pictureBox1.Width, pictureBox1.Height);
 		}
 
@@ -52,10 +63,21 @@ namespace RoomEditor {
 			}
 		}
 
-		//delete button
+		private void addToListbox(Tiles tileName, SpriteSheetForm sheet) {
+			if(sheet != null) {
+				listBox1.Items.Add(sheet.CurrentTile);
+				DrawRoom();
+			}
+		}
+
+		//delete button - make work with new stuff
 		private void button2_Click(object sender, EventArgs e) {
 			if(listBox1.SelectedIndex >= 0 && listBox1.SelectedIndex < listBox1.Items.Count) {
 				listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+				//drawSpriteAtPos.RemoveAt(listBox1.SelectedIndex);
+
+
+
 				DrawRoom();
 			}
 		}
@@ -83,25 +105,96 @@ namespace RoomEditor {
 					}
 				}
 			}
-
 			return null;
 		}
 
 		private void DrawRoom() {
+			drawGrid();
 			Graphics graphics = Graphics.FromImage(drawArea);
-			graphics.FillRectangle(Brushes.White, 0, 0, drawArea.Width, drawArea.Height);
-			Rectangle destination = new Rectangle(0, 0, spritesheet.GridWidth<<2, spritesheet.GridHeight<<2);
 
-			foreach(Point tile in listBox1.Items) {
+			//change this to selectes coord
+			//foreach(Point tile in listBox1.Items) {
+			//	Rectangle source = new Rectangle(
+			//		tile.X * (spritesheet.GridWidth + spritesheet.Spacing),
+			//		tile.Y * (spritesheet.GridHeight + spritesheet.Spacing),
+			//		spritesheet.GridWidth,
+			//		spritesheet.GridHeight);
+			//	Rectangle destination = new Rectangle(tile.X * (spritesheet.GridWidth + spritesheet.Spacing), tile.Y * (spritesheet.GridHeight + spritesheet.Spacing), spritesheet.GridWidth, spritesheet.GridHeight);
+			//	graphics.DrawImage(spritesheet.Image, destination, source, GraphicsUnit.Pixel);
+			//}
+
+			foreach(Tiles tile in drawSpriteAtPos) {
 				Rectangle source = new Rectangle(
-					tile.X * (spritesheet.GridWidth + spritesheet.Spacing),
-					tile.Y * (spritesheet.GridHeight + spritesheet.Spacing),
+					tile.SourceCoord.X * (spritesheet.GridWidth + spritesheet.Spacing),
+					tile.SourceCoord.Y * (spritesheet.GridHeight + spritesheet.Spacing),
 					spritesheet.GridWidth,
 					spritesheet.GridHeight);
+				Rectangle destination = new Rectangle(tile.DestinationCoord.X * (spritesheet.GridWidth + spritesheet.Spacing), tile.DestinationCoord.Y * (spritesheet.GridHeight + spritesheet.Spacing), spritesheet.GridWidth, spritesheet.GridHeight);
 				graphics.DrawImage(spritesheet.Image, destination, source, GraphicsUnit.Pixel);
 			}
 			graphics.Dispose();
 			pictureBox1.Image = drawArea;
+		}
+
+		//draw grid over the sprite area
+		private void drawGrid() {
+			pictureBox1.DrawToBitmap(drawArea, pictureBox1.Bounds);
+
+			Graphics graphics;
+			graphics = Graphics.FromImage(drawArea);
+			graphics.Clear(Color.FromArgb(0, 255, 255, 255));
+
+			if(spritesheet == null) {
+				return;
+			}
+
+			Pen pen = new Pen(Brushes.Black);
+
+			int height = pictureBox1.Height;
+			int width = pictureBox1.Width;
+			for(int y = 0; y < height; y += spritesheet.GridHeight + spritesheet.Spacing) {
+				graphics.DrawLine(pen, 0, y, width, y);
+			}
+
+			for(int x = 0; x < width; x += spritesheet.GridWidth + spritesheet.Spacing) {
+				graphics.DrawLine(pen, x, 0, x, height);
+			}
+
+			Pen highlight = new Pen(Brushes.Red);
+			graphics.DrawRectangle(highlight, CurrentTile.X * (spritesheet.GridWidth + spritesheet.Spacing), CurrentTile.Y * (spritesheet.GridHeight + spritesheet.Spacing), spritesheet.GridWidth + spritesheet.Spacing, spritesheet.GridHeight + spritesheet.Spacing);
+
+			graphics.Dispose();
+			pictureBox1.Image = drawArea;
+		}
+
+		//clicking picbox will grag coord and let you dump image at that point
+		private void pictureBox1_Click(object sender, EventArgs e) {
+			if(spritesheet == null) {
+				return;
+			}
+
+			if(e.GetType() == typeof(MouseEventArgs)) {
+				MouseEventArgs mouse = e as MouseEventArgs;
+				CurrentTile = new Point(mouse.X / (spritesheet.GridWidth + spritesheet.Spacing), mouse.Y / (spritesheet.GridHeight + spritesheet.Spacing));
+
+				//save tile coord here for draw use
+				Tiles tmp = new Tiles();
+				tmp.DestinationCoord = new Point(CurrentTile.X, CurrentTile.Y);
+
+				SpriteSheetForm sheet = FindSheet();
+				if(sheet != null) {
+					tmp.SourceCoord = new Point(sheet.CurrentTile.X, sheet.CurrentTile.Y);
+				} else {
+					tmp.SourceCoord = new Point(0, 0);
+				}
+				drawSpriteAtPos.Add(tmp);
+				addToListbox(tmp, sheet);
+				DrawRoom();
+			}
+		}
+
+		private void RoomForm_Shown(object sender, EventArgs e) {
+			drawGrid();
 		}
 	}
 }
